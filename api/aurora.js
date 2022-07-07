225b7769e061de421ca17da4a2953a5a6ef85192
Owen Stubbs <owen77stubbs@gmail.com>
2022-07-07 18:14:45 +0100

    DDD

--- api/aurora.js
+++ api/aurora.js
@@ -4,18 +4,15 @@ const output = async data => {
     switch(data.toLowerCase()) {
         case 'towns': return await Aurora.getTowns()
         case 'nations': return await Aurora.getNations()
-        default: return null
+        default: return `Data parameter ${data} not recognized.`
     }
 }
 
 async function handler(req, res) {
-    //const { data } = req.query
-    //if (!data) return res.status(404).send('Error: Data not specified.')
+    const { data } = req.query
+    if (!data) return res.status(404).send('Error: Data not specified.')
 
-    let out = await output('towns')
-    if (!out) return res.status(200).send(`Data parameter ${data} not recognized.`)
-
-    res.status(200).json(out)
+    res.status(200).json(await output(data))
 }
 
 export {
