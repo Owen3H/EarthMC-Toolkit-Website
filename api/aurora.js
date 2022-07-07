74c7dc6552f304c7d826702623686d4439d771fd
Owen Stubbs <owen77stubbs@gmail.com>
2022-07-07 15:17:45 +0100

    Update aurora.js

--- api/aurora.js
+++ api/aurora.js
@@ -4,7 +4,7 @@ module.exports = async (req, res) => {
 
     if (data.toLowerCase() == 'nations') {
         let nations = await emc.Aurora.getNations()
-        res.status(200).json(nations)
+        return res.status(200).json(nations)
     }
 
     return res.status(404).send('Error: Data not specified.')
