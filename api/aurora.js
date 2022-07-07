cc9d05ab0ec973b172bb8526de9165c144be62bb
Owen Stubbs <owen77stubbs@gmail.com>
2022-07-07 17:22:45 +0100

    Update aurora.js

--- api/aurora.js
+++ api/aurora.js
@@ -1,17 +1,18 @@
 const { Aurora } = require('earthmc')
 
-module.exports = async (req, res) => {
-    function output(data) {
-        switch(data.toLowerCase()) {
-            case 'towns': return await Aurora.getTowns()
-            case 'nations': return await Aurora.getNations()
-            default: return `Data parameter ${data} not recognized.`
-        }
+const output = async data => {
+    switch(data.toLowerCase()) {
+        case 'towns': return await Aurora.getTowns()
+        case 'nations': return await Aurora.getNations()
+        default: return `Data parameter ${data} not recognized.`
     }
+}
 
+async function handler(req, res) {
     const { data } = req.query
     if (!data) return res.status(404).send('Error: Data not specified.')
 
-    let output = output(data)
-    res.status(200).json(output)
+    res.status(200).json(await output(data))
 }
+
+export default handler
