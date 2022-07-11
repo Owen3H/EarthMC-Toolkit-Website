627b78c326b68d8873bdedfe85cafb0fc2b399c9
Owen Stubbs <owen77stubbs@gmail.com>
2022-07-11 20:33:50 +0100

    add 2 routes and site qol

--- utils/route.js
+++ utils/route.js
@@ -1,14 +1,24 @@
 const getData = async (params, map) => {
-    const [dataType, single] = params,
+    const [dataType, single, filter] = params,
           value = single?.toLowerCase() ?? null
 
     switch(dataType) {
-        case 'towns': return single ? await map.getTown(value) : await map.getTowns()  
-        case 'nations': return single ? await map.getNation(value) : await map.getNations()
+        case 'towns': {
+            if (!single) return await map.getTowns()
+            if (!filter) return await map.getTown(value)
+
+            return filter.toLowerCase() == "joinable" ? await map.getJoinableNations(value) : `Parameter ${filter} not recognized.`
+        }
+        case 'nations': {
+            if (!single) return await map.getNations()
+            if (!filter) return await map.getNation(value)
+
+            return filter.toLowerCase() == "invitable" ? await map.getInvitableTowns(value, false) : `Parameter ${filter} not recognized.`
+        }
         case 'allplayers': return single ? await map.getPlayer(value) :await map.getAllPlayers()
         case 'residents': return single ? await map.getResident(value) : await map.getResidents()
         case 'onlineplayers': return single ? await map.getOnlinePlayer(value) : await map.getOnlinePlayers(true)
-        default: return null
+        default: return `Parameter ${dataType} not recognized.`
     }
 }
 
@@ -16,7 +26,7 @@ async function serve(req, res, map) {
     let { params } = req.query,
         out = await getData(params, map)
         
-    if (!out) return res.status(400).send(`Parameter ${data} not recognized.`)
+    if (!out) return res.status(400).send(out)
     
     res.setHeader('Cache-Control', 's-maxage=30, stale-while-revalidate')
     res.status(200).json(out)
