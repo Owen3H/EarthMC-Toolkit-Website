e728300fa8d5b894a6adbe712e12e56650283a1e
Owen Stubbs <owen77stubbs@gmail.com>
2022-08-07 01:19:17 +0100

    increase body size limit

--- pages/api/[param].js
+++ pages/api/[param].js
@@ -1,2 +1,10 @@
 const serve = require('../../utils/generic').default
+
 export default (req, res) => serve(req, res)
+export const config = {
+    api: {
+        bodyParser: {
+            sizeLimit: '4mb'
+        }
+    }
+}
--- pages/api/nova/[...params].js
+++ pages/api/nova/[...params].js
@@ -12,3 +12,10 @@ async function handler(req, res) {
 }
 
 export default handler
+export const config = {
+    api: {
+        bodyParser: {
+            sizeLimit: '4mb'
+        }
+    }
+}
